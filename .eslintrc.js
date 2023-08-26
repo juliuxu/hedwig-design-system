eab6cc66b446cb3239cb90ea090c44963af8cb6a
Julian Jark <juliuxu@gmail.com>
2023-08-26 17:33:21 +0200

    :construction: rename

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-acme`
-  extends: ["acme"],
+  // This tells ESLint to load the config from the package `eslint-config-hedwig-julian`
+  extends: ["hedwig-julian"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -14,10 +14,10 @@ module.exports = {
       resolve: {
         alias: [
           {
-            find: "@acme/core",
+            find: "@hedwig-julian/core",
             replacement: path.resolve(
               __dirname,
-              "../../../packages/acme-core/"
+              "../../../packages/core/"
             ),
           },
         ],

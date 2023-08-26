d294d69985679990272ea4ac11b623d139740fc3
Julian Jark <juliuxu@gmail.com>
2023-08-26 17:23:10 +0200

    :construction: configure linting

--- packages/eslint-config-acme/index.js
+++ packages/eslint-config-acme/index.js
@@ -1,11 +1,52 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  plugins: ["simple-import-sort"],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
+    // Enforces no extra curly braces around string props or children
+    "react/jsx-curly-brace-presence": [
+      2,
+      {
+        props: "never",
+        children: "never",
+        propElementValues: "always",
+      },
+    ],
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          // Load imports with side-effects first, because duh
+          ["^\\u0000"],
+          // Other npm imports
+          ["^@?\\w"],
+          // Relative imports
+          ["^~", "^\\."],
+        ],
+      },
+    ],
   },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
+  ignorePatterns: [
+    "build/*",
+    "node_modules/*",
+    "playwright-report/*",
+    "test-results/*",
+    ".cache/*",
+    ".github/*",
+  ],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
     },
   },
 };

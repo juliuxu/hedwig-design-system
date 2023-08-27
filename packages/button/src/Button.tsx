fb287b72fda46d943a8acfe0d8a0d311c6a043ab
Julian Jark <juliuxu@gmail.com>
2023-08-27 20:34:40 +0200

    wip

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -24,7 +24,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       fullWidthOnMobile = false,
       disabled,
       ...rest
-    }: ButtonProps,
+    },
     ref
   ) => (
     <button
